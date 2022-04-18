0dee42ecff3eec585ebf1b220f942ddc82fb52f2
tanjuntao <13719009892@163.com>
2022-04-18 23:19:25 +0800

    login done(not reg and post ect)

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,4 +10,23 @@ $(function () {
         $('.login-box').show()
         $('.reg-box').hide()
     })
+
+    // 从layui中获取form对象
+    var form = layui.form
+    // 自定义规则
+    form.verify({
+        // 自定义了一个密码的校验规则
+        password: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+        ],
+        repassword: function (value) { 
+            // 通过形参获得内容，比较方式来判断是否一致
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd != value) { 
+                return '两次密码不一致'
+            }
+        }
+
+    })
 })
